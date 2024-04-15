30d36695991b18915fc5fa938f5d190aaaa0f4e5
Марк <kareev_mark@mail.ru>
2024-04-15 15:18:47 +0700

    Testovoe

--- src/Minmax.java
+++ src/Minmax.java
@@ -1,21 +1,21 @@
 public class Minmax {
     public static void main(String[] args) {
 
-        int n = 100;
-        double[] array = new double[n];
-        for (int i = 0; i < array.length; i++) {
+        final int SIZE = 100 = 100;
+        double[] array = new double[SIZE];
+        for (int i = 0; i < SIZE; i++) {
             array[i] = Math.random();
         }
 
         double max = array[0];
         double min = array[0];
         double avg = 0;
-        for (int i = 0; i < array.length; i++) {
+        for (int i = 0; i <SIZE; i++) {
             if (max < array[i])
                 max = array[i];
             if (min > array[i])
                 min = array[i];
-            avg += array[i] / array.length;
+            avg += array[i] / SIZE;
         }
 
         System.out.println("Максимальное значение = " + max);
